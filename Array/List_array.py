95d09b61f0e59658f8f8597efe6d60062e8a6523
Aastha Agarwal <astha1916@gmail.com>
2022-12-11 23:45:22 +0530

    Array Implementation

---
+++ Array/List_array.py
@@ -0,0 +1,9 @@
+#list array
+
+import array as arr  #for integer value only
+list1=arr.array('i',[3,6,4,9]) #--> i: integers  Thus it wil store only integer values
+#list2=arr.array('f',[45,76.58,95.598])
+list1.append(57)
+print(list1)
+#print(dir(list1))
+#print(list2)
