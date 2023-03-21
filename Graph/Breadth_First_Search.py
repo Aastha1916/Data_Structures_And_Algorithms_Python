f098d89e812907bf7319a1ed83538746deab8bfa
Aastha Agarwal <astha1916@gmail.com>
2023-03-21 18:30:41 +0530

    Printing bfs traversal

--- Graph/Breadth_First_Search.py
+++ Graph/Breadth_First_Search.py
@@ -4,14 +4,15 @@ source = 1
 edge = [[1,2], [1,4], [2,3], [2, 5], [4, 5], [3, 5]]
 adjlist = [[] for i in range(n+1)]
 for i in range(e):
-    adjlist[adjlist[i][0].append(edge[i][1])]
-    adjlist[adjlist[i][1].append(edge[i][0])]
+    adjlist[edge[i][0]].append(edge[i][1])
+    adjlist[edge[i][1]].append(edge[i][0])
 
 d = [0]*(n+1)
 s = [0]*(n+1)
 q = [source]
+ans = [source]
 s[source] = 1
-while (len(q)>0):
+while q:
     x = q.pop(0)
     l = adjlist[x]
     for i in l:
@@ -19,5 +20,7 @@ while (len(q)>0):
             s[i] = 1
             d[i] = d[x] + 1
             q.append(i)
+            ans.append(i)
 
 print(d[1:])
+print(ans)
