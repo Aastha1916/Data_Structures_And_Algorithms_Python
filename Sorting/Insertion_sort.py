39261a7d9e9dc25d63e355fdb541607089ccb6dd
Aastha Agarwal <astha1916@gmail.com>
2023-08-09 10:30:14 +0530

    Code modified

--- Sorting/Insertion_sort.py
+++ Sorting/Insertion_sort.py
@@ -1,24 +1,32 @@
+# Iterative Approach
 
 def insertsort(l):
-    for i in range(len(l)):
-        pos=i
-        while pos>0 and l[pos]<l[pos-1]:
-            l[pos],l[pos-1]=l[pos-1],l[pos] 
-            pos=pos-1
+    for i in range(1,len(l)):
+        key = l[i]
+        j = i-1
+        while j >= 0 and l[j] > key:
+            l[j+1] = l[j]
+            j-=1
+        l[j+1] = key
+        # print(l)
     return l
 
 print(insertsort([23,44,1,4,66,7]))
 
 #-----------------------------------
+# Recursive Approach
 
-def insertsort(l,start,n):
-    if start>=n-1:
-        return
-    pos=start
-    while pos>0 and l[pos]<l[pos-1]:
-        l[pos],l[pos-1]=l[pos-1],l[pos] 
-        pos=pos-1
-    insertsort(l, start, n)
+def insertsort(l,n):
+    if n<=1:
+        return l
+    insertsort(l, n-1)
+    key = l[n-1]
+    j = n-2
+    while j >= 0 and l[j] > key:
+        l[j+1] = l[j]
+        j-=1
+    l[j+1] = key
+    
 arr=[23,44,1,4,66,7]
-insertsort(arr,0,len(arr))
+insertsort(arr,len(arr))
 print(arr)
