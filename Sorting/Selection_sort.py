ec8b0bbd2e781ee3fc8f0212f29884537d7f8a0d
Aastha Agarwal <astha1916@gmail.com>
2023-08-17 14:25:24 +0530

    Adding selection sorting

--- Sorting/Selection_sort.py
+++ Sorting/Selection_sort.py
@@ -1,15 +1,17 @@
 
 def selection(arr):
-    for i in range(len(arr)):
+    for i in range(len(arr)-1):
         minindex=i
         for j in range(i+1,len(arr)):
             if arr[j]<arr[minindex]:
                 minindex=j
         arr[i],arr[minindex]=arr[minindex],arr[i]
-    return arr
+    
 
 arr=[23,43,67,1,44,6,78,9,0,2]
-print(selection(arr))
+selection(arr)
+print(arr)
+
 
 
 #T(n)=T(n-1)+n------------------------------------
