7d3bc163ed23c3489e01df53c168a37badc6fe4b
Aastha Agarwal <astha1916@gmail.com>
2023-09-04 17:56:15 +0530

    Adding Instance, Class, Static method implementation

---
+++ OOPS_concepts/Instance_vs_class_vs_static_methods.py
@@ -0,0 +1,19 @@
+class MyClass:
+    def method(self):
+        return 'instance method called', self
+
+    @classmethod
+    def classmethod(cls):
+        return 'class method called', cls
+
+    @staticmethod
+    def staticmethod():
+        return 'static method called'
+
+obj = MyClass()
+print(obj.method())
+print(MyClass.method())
+print(obj.classmethod())
+print(MyClass.classmethod())
+print(obj.staticmethod())
+print(MyClass.staticmethod())
